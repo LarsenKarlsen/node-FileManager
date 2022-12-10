7fc7bf63a495d3751347835aed0b9ad2cffcc53e
A.Protasevich <a.protasevich93@gmail.com>
2022-12-10 12:34:54 +0300

    feat: welcome and goodbye msg

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-console.log('Index.js')
+import fs from 'fs';
+import os from 'os';
+import readline from 'readline';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+
+const fileManager = async () => {
+    const homedir = os.homedir();
+    let currentdir = homedir;
+
+    const username = process.argv.slice(2).filter(s => s.includes('--username'))[0].split('=')[1];  
+    console.log(`Welcome to the File Manager, ${username}!`);
+
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        prompt: 'Enter text msg : => ',
+    });
+
+    rl.prompt();
+
+    rl.on('line', (line)=>{
+        switch(line.trim()){
+            case 'exit':
+                rl.close();
+                break;
+            default:
+                console.log(line)
+                rl.prompt();
+                break;
+        }
+    }).on('close', ()=>{
+        console.log(`\nThank you for using File Manager, ${username}, goodbye!`)
+    })
+
+
+
+};
+
+await fileManager();
