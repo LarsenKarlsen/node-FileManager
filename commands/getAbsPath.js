35bd8dafaa3f8f57e1d75fe2c8879432845859d2
A.Protasevich <a.protasevich93@gmail.com>
2022-12-17 12:39:58 +0300

    fix: getAbsPath function

--- commands/getAbsPath.js
+++ commands/getAbsPath.js
@@ -3,7 +3,11 @@ import path from 'path';
 const getAbsPath = (pathToFile, currentdir) => {
     let absPathTo;
     if (!path.isAbsolute(pathToFile)) {
-        absPathTo = path.join(currentdir, pathToFile);
+        if(pathToFile.includes('./') || pathToFile.includes('../')) {
+            absPathTo = path.resolve(currentdir, pathToFile);
+        } else {
+            absPathTo = path.join(currentdir, pathToFile);
+        };
     } else {
         absPathTo = pathToFile;
     };
